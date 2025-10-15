4dad358f73b6004859a546bc2ef2d9bd9d3e668e
Kósa Áron <90419831+ar0nK@users.noreply.github.com>
2025-10-15 18:47:15 +0200

    --Untested-- Avoiding double code, stabilization, commenting in english
    Creating methods from repeating codes, making prototipes of the methods to avoid errors, commenting the new code in english that more people understands

--- DoorBell/Arduino_door.ino
+++ DoorBell/Arduino_door.ino
@@ -2,80 +2,68 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-// Configuración de WiFi
+// WIFI config
 const char* ssid = "Fablab_Torino";
 const char* password = "Fablab.Torino!";
 
-// Configuración de MQTT
+// MQTT config
 const char* mqtt_server = "172.26.34.36";
 const char* mqtt_topic_door1 = "Door1_topic";
 const char* mqtt_username = "tu_usuario";
 const char* mqtt_password = "tu_contraseña";
 
+// Network objects
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Variables para almacenar el estado de los LED
+// LED and relay config
+const int ledPinDoor1 = 13;
 bool ledStateDoor1 = false;
 unsigned long ledOnTimeDoor1 = 0;
 
-// Datos del timbre
-const char* hostname = "your esp-rfid hostname"; // Cambia esto por tu hostname
+const int relayPin = 4;
+unsigned long relayOnTime = 0;
 
-// Configuración del relé
-const int relayPin = 4; // Pin al que está conectado el relé (ajusta si es necesario)
-unsigned long relayOnTime = 0; // Para gestionar el tiempo de activación del relé
+// Creating the method prototipes before the setup and loop to make the code more stable
+void connectToWiFi();
+void connectToMQTT();
+void controlLED(int pin, bool& state, unsigned long& onTime, unsigned long duration);
+void triggerRelay(unsigned long duration = 3000);
+void publishJson(const char* topic, StaticJsonDocument<200>& doc);
+void sendDoorbellMessage(const char* doorName);
+void sendNotificationToPublisher(const char* message);
 
+//Setting up the wifi, mqtt, callback, LED and relay
 void setup() {
   Serial.begin(9600);
 
-  // Conexión a WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando a WiFi...");
-  }
-  Serial.println("Conectado a WiFi");
+  pinMode(ledPinDoor1, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 
-  // Conexión a MQTT
+  connectToWiFi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
-  while (!client.connected()) {
-    Serial.println("Conectando a MQTT...");
-    if (client.connect("ArduinoClient", mqtt_username, mqtt_password)) {
-      Serial.println("Conectado a MQTT");
-      client.subscribe(mqtt_topic_door1);
-    } else {
-      Serial.print("Error al conectar: ");
-      Serial.print(client.state());
-      delay(2000);
-    }
-  }
-
-  // Inicializar los pines de los LED
-  pinMode(13, OUTPUT);
-  pinMode(relayPin, OUTPUT); // Configurar el pin del relé como salida
-  digitalWrite(relayPin, LOW); // Asegurarse de que el relé está apagado al inicio
+  connectToMQTT();
 }
 
 void loop() {
   if (!client.connected()) {
-    reconnect();
+    connectToMQTT();
   }
   client.loop();
 
-  // Controlar el LED de la puerta 1 según su estado
-  controlLED(13, ledStateDoor1, ledOnTimeDoor1);
+  controlLED(ledPinDoor1, ledStateDoor1, ledOnTimeDoor1, 5000);
 
-  // Controlar el relé, apagar después de 3 segundos
-  if (millis() - relayOnTime >= 3000 && relayOnTime > 0) { // Después de 3 segundos
-    digitalWrite(relayPin, LOW); // Apagar el relé
-    Serial.println("Relé apagado"); // Imprimir mensaje cuando el relé se apaga
-    relayOnTime = 0; // Resetear el tiempo
+  //Triggering the relay
+  if (relayOnTime > 0 && millis() - relayOnTime >= 3000) {
+    digitalWrite(relayPin, LOW);
+    Serial.println("Relé apagado");
+    relayOnTime = 0;
   }
 }
 
+//Callback method to react messages
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Mensaje recibido [");
   Serial.print(topic);
@@ -85,74 +73,79 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  // Control de los LED basado en el topic del mensaje recibido
+  //Turniing on the relay for 3 sec 
   if (strcmp(topic, mqtt_topic_door1) == 0) {
     ledStateDoor1 = true;
     ledOnTimeDoor1 = millis();
-    sendDoorbellMessage("Door 1"); // Enviar mensaje del timbre
-    sendNotificationToPublisher("Door 1 opened"); // Enviar notificación al publicador
-
-    // Activar el relé para encender las luces (12V) durante 3 segundos
-    digitalWrite(relayPin, HIGH); // Encender el relé (12V)
-    Serial.println("Relé activado"); // Imprimir mensaje cuando el relé se activa
-    relayOnTime = millis(); // Registrar el tiempo de activación del relé
+    sendDoorbellMessage("Door 1");
+    sendNotificationToPublisher("Door 1 opened");
+    triggerRelay();  
   } else {
     ledStateDoor1 = false;
   }
 }
 
-void sendDoorbellMessage(const char* doorName) {
-  unsigned long currentTime = millis() / 1000; // Tiempo en segundos
-  StaticJsonDocument<200> jsonDoc; // Crear un documento JSON
-  
-  jsonDoc["type"] = "INFO"; // Tipo de mensaje
-  jsonDoc["src"] = doorName; // Nombre de la puerta
-  jsonDoc["desc"] = "Doorbell ringing"; // Descripción del evento
-  jsonDoc["data"] = ""; // Datos adicionales (vacío en este caso)
-  jsonDoc["time"] = currentTime; // Marca de tiempo
-  jsonDoc["cmd"] = "event"; // Comando relacionado
-  
-  // Asignar el nombre del host basado en el nombre de la puerta
-  String topicName = String(doorName) + "_topic"; 
-  jsonDoc["hostname"] = topicName; // Nombre del host
-
-  String jsonString;
-  serializeJson(jsonDoc, jsonString); // Serializar el documento JSON a una cadena
-
-  Serial.println(jsonString); // Imprimir mensaje en el monitor serial
-  client.publish("door/events", jsonString.c_str()); // Publicar el mensaje en el tópico MQTT
+//Method to connect to wifi
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a WiFi...");
+  }
+  Serial.println("Conectado a WiFi");
 }
 
-void sendNotificationToPublisher(const char* message) {
-  StaticJsonDocument<200> jsonDoc; // Crear un documento JSON para la notificación
-  
-  jsonDoc["type"] = "INFO"; // Tipo de mensaje
-  jsonDoc["message"] = message; // Mensaje de notificación
-
-  String jsonString;
-  serializeJson(jsonDoc, jsonString); // Serializar el documento JSON a una cadena
-
-  // Publicar el mensaje en el tópico de notificaciones
-  client.publish("door/notifications", jsonString.c_str());
-}
-
-void reconnect() {
+//Metehod to connect to mqtt
+void connectToMQTT() {
   while (!client.connected()) {
-    Serial.println("Reconectando a MQTT...");
+    Serial.println("Conectando a MQTT...");
     if (client.connect("ArduinoClient", mqtt_username, mqtt_password)) {
-      Serial.println("Conectado");
+      Serial.println("Conectado a MQTT");
       client.subscribe(mqtt_topic_door1);
     } else {
-      Serial.print("Error al reconectar: ");
-      Serial.print(client.state());
-      delay(5000);
+      Serial.print("Error al conectar: ");
+      Serial.println(client.state());
+      delay(2000);
     }
   }
 }
-
-void controlLED(int ledPin, bool& ledState, unsigned long& ledOnTime) {
-  if (ledState && (millis() - ledOnTime >= 5000)) { // Si el LED está encendido y han pasado 5 segundos
-    ledState = false; // Apagar el LED
+//Method to control LED 
+void controlLED(int pin, bool& state, unsigned long& onTime, unsigned long duration) {
+  if (state && millis() - onTime >= duration) {
+    state = false;
   }
-  digitalWrite(ledPin, ledState);
+  digitalWrite(pin, state);
+}
+
+//Method to trigger the relay
+void triggerRelay(unsigned long duration) {
+  digitalWrite(relayPin, HIGH);
+  Serial.println("Relé activado");
+  relayOnTime = millis();  
+}
+//Method to convert to json and send it to the client
+void publishJson(const char* topic, StaticJsonDocument<200>& doc) {
+  String jsonString;
+  serializeJson(doc, jsonString);
+  client.publish(topic, jsonString.c_str());
+  Serial.println(jsonString);
+}
+//Making the doorbell's message 
+void sendDoorbellMessage(const char* doorName) {
+  StaticJsonDocument<200> doc;
+  doc["type"] = "INFO";
+  doc["src"] = doorName;
+  doc["desc"] = "Doorbell ringing";
+  doc["data"] = "";
+  doc["time"] = millis() / 1000;
+  doc["cmd"] = "event";
+  doc["hostname"] = String(doorName) + "_topic";
+  publishJson("door/events", doc);
+}
+//Sending the message to the publisher
+void sendNotificationToPublisher(const char* message) {
+  StaticJsonDocument<200> doc;
+  doc["type"] = "INFO";
+  doc["message"] = message;
+  publishJson("door/notifications", doc);
 }
