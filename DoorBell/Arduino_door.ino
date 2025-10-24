9f9189f880c5ace3f408fee17bd9de707ac60ecf
Kósa Áron <90419831+ar0nK@users.noreply.github.com>
2025-10-24 16:55:18 +0200

    Deleted useless functions

--- DoorBell/Arduino_door.ino
+++ DoorBell/Arduino_door.ino
@@ -15,27 +15,19 @@ const char* mqtt_topic_door1 = "Door1_topic";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// LED and relay config
-const int ledPinDoor1 = 13;
-bool ledStateDoor1 = false;
-unsigned long ledOnTimeDoor1 = 0;
-
+//Relay initialisation
 const int relayPin = 4;
 unsigned long relayOnTime = 0;
 
 // Function prototypes
 void connectToWiFi();
 void connectToMQTT();
-void controlLED(int pin, bool& state, unsigned long& onTime, unsigned long duration);
 void triggerRelay(unsigned long duration = 3000);
 void callback(char* topic, byte* payload, unsigned int length);
-void publishJson(const char* topic, StaticJsonDocument<200>& doc);
-void sendDoorbellMessage(const char* doorName);
-void sendNotificationToPublisher(const char* message);
 
 // Setup
 void setup() {
-  pinMode(ledPinDoor1, OUTPUT);
+  
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 
@@ -47,12 +39,17 @@ void setup() {
 
 // Main loop
 void loop() {
+  //Wifi reconnection
+   if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+  }
+
   if (!client.connected()) {
     connectToMQTT();
   }
   client.loop();
 
-  controlLED(ledPinDoor1, ledStateDoor1, ledOnTimeDoor1, 5000);
+  
 
   // Turn off relay after 3 seconds
   if (relayOnTime > 0 && millis() - relayOnTime >= 3000) {
@@ -99,44 +96,10 @@ void connectToMQTT() {
   }
 }
 
-// LED control
-void controlLED(int pin, bool& state, unsigned long& onTime, unsigned long duration) {
-  if (state && millis() - onTime >= duration) {
-    state = false;
-  }
-  digitalWrite(pin, state);
-}
+
 
 // Relay trigger
 void triggerRelay(unsigned long duration) {
   digitalWrite(relayPin, HIGH);
   relayOnTime = millis();
-}
-
-// Publish JSON
-void publishJson(const char* topic, StaticJsonDocument<200>& doc) {
-  String jsonString;
-  serializeJson(doc, jsonString);
-  client.publish(topic, jsonString.c_str());
-}
-
-// Send doorbell event
-void sendDoorbellMessage(const char* doorName) {
-  StaticJsonDocument<200> doc;
-  doc["type"] = "INFO";
-  doc["src"] = doorName;
-  doc["desc"] = "Doorbell ringing";
-  doc["data"] = "";
-  doc["time"] = millis() / 1000;
-  doc["cmd"] = "event";
-  doc["hostname"] = String(doorName) + "_topic";
-  publishJson("door/events", doc);
-}
-
-// Send notification
-void sendNotificationToPublisher(const char* message) {
-  StaticJsonDocument<200> doc;
-  doc["type"] = "INFO";
-  doc["message"] = message;
-  publishJson("door/notifications", doc);
 }
